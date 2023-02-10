d83e2c3ad9ebca048ac4ca98860f9d180873515e
DDIONG <12yangyang25@naver.com>
2023-02-10 13:53:19 +0900

    PHPhotoLibraryChangeObserver - 사진 삭제하기

--- PhotosExample/PhotosExample/ViewController.swift
+++ PhotosExample/PhotosExample/ViewController.swift
@@ -2,7 +2,7 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,PHPhotoLibraryChangeObserver {
     
     @IBOutlet weak var tableView: UITableView!
     var fetchResult: PHFetchResult<PHAsset>!
@@ -22,7 +22,7 @@ class ViewController: UIViewController, UITableViewDataSource {
             Options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
             return Options
         }()
-
+        
         fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
         
     }
@@ -58,7 +58,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         default:
             break
         }
-        
+        PHPhotoLibrary.shared().register(self)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -74,6 +74,31 @@ class ViewController: UIViewController, UITableViewDataSource {
         }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let asset: PHAsset = self.fetchResult[indexPath.row]
+            
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetChangeRequest.deleteAssets([asset] as NSArray)}
+                                                   , completionHandler: nil)
+        }
+    }
+        
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult) else{
+            return
+        }
+        
+        fetchResult = changes.fetchResultAfterChanges
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
+        }
     }
     
 }
